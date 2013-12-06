a2f0c324e03d2f9fd1a4ef0e8284c2ff6a2e4148
Waseem Ahmad <waseem@rice.edu>
2013-12-05 19:55:05 -0600

    Created setup file

--- src/setup.py
+++ src/setup.py
@@ -2,5 +2,10 @@
 Run this file once to setup your database.
 """
 
-
 from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+
+db = client.main    # Our app uses main database
+
+db.serveries.add( {})
