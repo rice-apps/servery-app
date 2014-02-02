706d9f27d1c7e2ebbb0a87d855e408891b500acc
Waseem Ahmad <waseem@rice.edu>
2014-02-02 15:31:35 -0600

    Changed the website theme to Flatly from Bootswatch

--- src/serveries.py
+++ src/serveries.py
@@ -3,6 +3,9 @@ from bson.json_util import dumps
 
 @app.route('/api/serveries')
 def get_serveries():
+  print "This should work"
   # Query mongo db for all serveries
   serveries = mongo.db.serveries.find()
   return dumps(serveries), 200, {"Content-Type" : "application/json"}
+
+print "Api serveries loaded"
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,20 +4,11 @@ angular.module('serveryApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'static/views/login.html',
+        templateUrl: 'static/views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/active', {
-        templateUrl: 'static/views/login.html',
-        controller: 'MainCtrl'
-      })
-      .when('/completed', {
-        templateUrl: 'static/views/login.html',
-        controller: 'MainCtrl'
-      })
-      .when('/serveries', {
-        templateUrl: 'static/views/serveries.html',
-        controller: 'ServeriesCtrl'
+      .when('/template', {
+        templateUrl: 'static/views/template.html'
       })
       .otherwise({
         redirectTo: '/'
