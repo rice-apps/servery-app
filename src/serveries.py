2745ca56af5b622446b8e79fefd434c4f0f4b4a5
= <=>
2014-02-09 13:24:28 -0600

    Added CAS login info

--- src/serveries.py
+++ src/serveries.py
@@ -4,5 +4,6 @@ from bson.json_util import dumps
 @app.route('/api/serveries')
 def get_serveries():
   # Query mongo db for all serveries
+  print "ran"
   serveries = mongo.db.serveries.find()
   return dumps(serveries), 200, {"Content-Type" : "application/json"}
