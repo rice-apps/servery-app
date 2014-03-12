2c5cbf11afb64fb9a61d1eb4dbbb09e66af37c3e
Waseem Ahmad <waseem@rice.edu>
2014-03-11 23:23:36 -0500

    Fixed serving of index.html

--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
 from app import app
-from flask import redirect, jsonify, request, session, send_from_directory
+from flask import redirect, jsonify, request, session
 
 import json
 import random
 import auth
 
 @app.route('/')
-def main():
-  print "Testing"
-  return send_from_directory('static', 'index.html')
-
-@app.route('/<path:filename>')
-def send_file(filename):
-  return send_from_directory('/static', filename)
+def root():
+  return app.send_static_file('index.html')
 
 @app.route('/auth/login-response')
 def get_login():
