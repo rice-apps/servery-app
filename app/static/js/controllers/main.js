f7d322aaa2eb0d0f2c41ede4160c23f3fd90f288
lwen2027 <lwen2027@gmail.com>
2014-03-18 21:26:53 -0500

    added pagecontent

--- app/static/js/controllers/main.js
+++ app/static/js/controllers/main.js
@@ -30,14 +30,45 @@ angular.module('serveryApp')
   ApiStub.serveries().then(function (data) {
     $scope.serveries = data;
     $scope.selectedServery = $scope.serveries[0];
+ 
   }, function (error) {
     alert(error);
   });
 
   $scope.selectServery = function (servery) {
     $scope.selectedServery = servery;
+    $scope.selectedPeriod = openhours;
     console.log("Selected servery: " + servery.name);
+   
   };
+
+  $scope.changeDate = function (date_num) {
+    if (date_num === 0){
+      return("Sunday");
+    }
+    else if (date_num===1){
+      return("Monday");
+    }
+    else if (date_num === 2){
+      return("Tuesday");
+    }
+    else if (date_num === 3){
+      return("Wednesday");
+    }
+    else if (date_num === 4){
+      return("Thursday");
+    }
+    else if (date_num === 5){
+      return("Friday");
+    }
+    else if (date_num === 6){
+      return("Saturday");
+    }
+  }
+
+  // $scope.changeDate = function(date) {
+  //   return $filter('date')(date, date:'hh:mm')
+  // }
 
 });
 
