affb236104592c26db96d7a137947a2ec401ae9f
Ethan Steinberg <ethan.steinberg@gmail.com>
2014-04-20 12:01:00 -0500

    Refactor date parsing

--- app/serveries.py
+++ app/serveries.py
@@ -3,11 +3,11 @@ from app import app
 import json
 from datetime import datetime
 import json
-from util import current_rice_time
+from util import current_rice_time, parse_to_rice_time
 
 from models import *
 
-from dateutil.parser import parse
+
 
 def find_mealtime(servery,day_of_the_week,meal_type):
     return db.session.query(MealTime).filter(MealTime.servery==servery,MealTime.day_of_the_week == day_of_the_week,MealTime.meal_type == meal_type).first()
@@ -55,7 +55,6 @@ def get_servery(servery_id):
   # retrieves actual servery
   servery = db.session.query(Servery).get(servery_id) 
 
-          
   open_filter = db.and_(MealTime.day_of_the_week == day_of_the_week,MealTime.start_time <= time,MealTime.end_time >= time)
 
   currently_open = db.session.query(Servery).filter(Servery.id==servery_id).join(Servery.mealtimes).filter(open_filter)
@@ -73,26 +72,16 @@ def get_servery(servery_id):
 
 @app.route('/api/serveries/<servery_id>/menu')
 def get_menu(servery_id):
-  now = current_rice_time()
-
   servery = db.session.query(Servery).get(servery_id)
 
-  # Query for menu items of servery given date (default today) and meal (default lunch and dinner)
-  date = request.args.get("date")
-  print date
-
-  if not date:
-    date = now.date()
+  if request.args.get("date"):
+    date = parse_to_rice_time(request.args.get("date")).date()
   else:
-    dt = parse(date)
-    date = dt.date()
-    print repr(dt),repr(date)
+    date = current_rice_time.date()
 
   meal = request.args.get("meal")
   if not meal:
     meal = 'both'
-
-  print date, meal
 
   if meal == "both":
     query_meals = ["lunch","dinner"]
@@ -108,114 +97,9 @@ def get_menu(servery_id):
               MealTime.servery == servery)
 
   for meal_type in query_meals:
-    menu[meal_type] = map(lambda x: {'name':x.dish_description},meal_type_query(meal_type).one().dishes)
+    meal = meal_type_query(meal_type).first()
+    if meal:
+        menu[meal_type] = map(lambda x: {'name':x.dish_description},meal.dishes)
   
 
   return json.dumps(menu), 200, {"content-type" : "application/json"}
-
-
-# @app.route('/api/serveries/<servery_id>/menu')
-# def get_menu_stub(date=strftime("%Y-%m-%d"), meal="both"):
-#   # Query for menu items of servery given date (default today) and meal (default lunch and dinner)
-#   menu = [{
-#     "_id": 123,
-#     "name": "Mac and Cheese",
-#     "tags": ["gluten","soy","milk"], 
-#     "type": "main",
-#     "meal": "lunch",
-#     "date": "2014-03-10",
-#     "servery": 311
-#   },{
-#     "_id": 124,
-#     "name": "Golden Catfish with Tartar Sauce",
-#     "tags": ["gluten","soy","milk","eggs","fish"],
-#     "meal": "main",
-#     "meal": "lunch",
-#     "date": "2014-03-10",
-#     "servery": 331
-#   },{
-#     "_id": 125,
-#     "name": "Okra Garlic Tomato Stew",
-#     "tags": ["gluten","soy"],
-#     "type": "soup",
-#     "meal": "lunch",
-#     "date": "2014-03-10",
-#     "servery": 312
-#   },{
-#     "_id": 126,
-#     "name": "Cheesecake",
-#     "tags": ["gluten","soy","milk","eggs"],
-#     "type": "dessert",
-#     "meal": "dinner",
-#     "date": "2014-03-10",
-#     "servery": 341
-#   }]
-
-
-#   return json.dumps(menu), 200, {"content-type" : "application/json"}
-
-
-
-# @app.route('/api/serveries')
-# def get_serveries_stub():
-#   print "This should work"
-#   serveries = [
-#     {
-#       "name": "North Servery",
-#       "image": {
-#         "link": "./static/img/placeholder.jpeg"
-#       },
-#       "location": {
-#         "latitude":29.721883,
-#         "longitude":-95.396546
-#       },
-#     }, {
-#       "name": "West Colleges Servery",
-#       "image": {
-#         "link": "./static/img/placeholder.jpeg"
-#       },
-#       "location": {
-#         "latitude":29.721063,
-#         "longitude":-95.398481
-#       }
-#     }
-#   ]
-
-#   return json.dumps(serveries), 200, {"content-type" : "application/json"}
-
-# @app.route('/api/serveries/<servery_id>')
-# def get_servery_stub(servery_id):
-#   if servery_id != "123" and servery_id != "124":
-#     return not_found()
-
-#   servery = {
-#     "_id": servery_id,
-#     "opening_hours": {
-#       "open_now": False,
-#       "periods": [
-#         {
-#           "meal": "lunch",
-#           "open": {
-#             "day": 0,
-#             "time": "1130"
-#           },
-#           "close": {
-#             "day": 0,
-#             "time": "1400"
-#           }
-#         }, {
-#           "meal": "dinner",
-#           "open": {
-#             "day": 0,
-#             "time": "1700"
-#           },
-#           "close": {
-#             "day": 0,
-#             "time": "1900"
-#           }
-#         }
-#       ]
-#     }
-#   }
-
-#   return json.dumps(servery), 200, {"content-type" : "application/json"}
--- app/util.py
+++ app/util.py
@@ -1,9 +1,14 @@
 from pytz import timezone, utc
 from datetime import datetime
+from dateutil.parser import parse
 
 rice_time = timezone("US/Central")
 
 def current_rice_time():
     now = utc.localize(datetime.utcnow()).astimezone(rice_time)
     return now
+
+def parse_to_rice_time(text):
+    return parse(text).astimezone(rice_time)
+
 
