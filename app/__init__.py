799cd838ec35291ad15b86b6ca4c69227346d7b8
Ethan Steinberg <ethan.steinberg@gmail.com>
2014-06-02 18:28:11 -0500

    Add config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RA'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///foo.db"
+
+app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
@@ -12,5 +15,4 @@ import models
 import main
 import serveries
 import users
-
 
