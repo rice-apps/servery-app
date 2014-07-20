42769d05437161bcfa34befddfb5d80d82f0221d
Ethan Steinberg <ethan.steinberg@gmail.com>
2014-07-20 11:32:56 -0500

    Done except for initialization

--- app/static/js/controllers/quickView.js
+++ app/static/js/controllers/quickView.js
@@ -3,6 +3,44 @@
 angular.module('serveryApp')
 .controller('QuickViewCtrl', ['$scope','nextmeals', function($scope,nextmeals) {
 
+
+var selected = 0;
+
+function foo()
+{
+    console.log("foo");
+
+    var allOptions = $(".menuThing");
+
+    $(allOptions[selected]).addClass("selected");
+
+
+    for (var i = 0; i < allOptions.length; i++)
+    {
+        console.log(allOptions[i]);
+        var currentPanel = $(allOptions[i]);
+        var blah = function(arg){
+            $(allOptions[arg]).mouseenter(function()
+            {
+                console.log("Mouse enter " + arg);
+                setSelection(arg);
+            });
+        }(i);
+
+    }
+
+}
+
+function setSelection(index)
+{
+    var allOptions = $(".menuThing");
+
+    $(allOptions[selected]).removeClass("selected");
+    selected = index;
+    $(allOptions[selected]).addClass("selected");
+}
+
+
     var daysOfTheWeek = [
         'Sunday',
         'Monday',
@@ -12,6 +50,38 @@ angular.module('serveryApp')
         'Friday',
         'Saturday'
     ];
+
+  
+
+    function recalculateRotateOrSimple()
+    {
+        var numberOfMenus = nextmeals.meals.length;
+
+        console.log(numberOfMenus);
+
+        var neededSizeForSimple = numberOfMenus * 200 + 200;
+
+        var spaceAvailable = $(".oneLine").width()
+        console.log(spaceAvailable);
+        console.log(neededSizeForSimple);
+
+        if (neededSizeForSimple > spaceAvailable)
+        {
+            $("#topHeader").removeClass("simple");
+            $("#topHeader").addClass("rotate");
+        }
+        else
+        {
+            $("#topHeader").removeClass("rotate");
+            $("#topHeader").addClass("simple"); 
+        }
+    }
+
+    recalculateRotateOrSimple();
+
+    $(window).resize(recalculateRotateOrSimple);
+    foo();
+  
 
     $scope.mealOptions = nextmeals.meals;
     $scope.day = daysOfTheWeek[new Date(nextmeals.day).getUTCDay()];
--- app/static/js/testViewer.js
+++ app/static/js/testViewer.js
@@ -8,14 +8,13 @@ function foo()
 
     var allOptions = $(".menuThing");
 
+    $(allOptions[selected]).addClass("selected");
 
-    var x = 100;
+
     for (var i = 0; i < allOptions.length; i++)
     {
         console.log(allOptions[i]);
         var currentPanel = $(allOptions[i]);
-
-        currentPanel.css({left:x+"px",top:"100px",width:"200"});
         var blah = function(arg){
             $(allOptions[arg]).mouseenter(function()
             {
@@ -23,39 +22,16 @@ function foo()
                 setSelection(arg);
             });
         }(i);
-        x+= 200;
+
     }
 
-    $(allOptions[selected]).css({"z-index":20});
-}
-
-function update()
-{
-
-    var allOptions = $(".menuThing");
-    var x = 100;
-    for (var i = 0; i < allOptions.length; i++)
-    {
-        var currentPanel = $(allOptions[i]);
-
-        if (i !== selected)
-        {
-            currentPanel.css({left:x+"px",top:"100px",width:"200"});
-            x+= 200;
-        }
-        else if (i === selected)
-        {
-            currentPanel.css({left:x+"px",top:"70px", width:"400"});
-            x+= 400; 
-
-        }
-    }
 }
 
 function setSelection(index)
 {
     var allOptions = $(".menuThing");
 
+    $(allOptions[selected]).removeClass("selected");
     selected = index;
-    update();
+    $(allOptions[selected]).addClass("selected");
 }
