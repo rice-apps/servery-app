e3af899b0e97034b359b71984ae158ed7f972b5a
Ethan Steinberg <ethan.steinberg@gmail.com>
2014-09-05 08:31:19 -0500

    Forgot to add filterstore

---
+++ app/static/js/stores/filterstore.js
@@ -0,0 +1,42 @@
+dispatch.factory('FilterStore', [function() {
+
+    var FilterStoreEvents = new EventEmitter();
+    var filters = {};
+
+
+    return {
+        addListener: function (callback){
+            FilterStoreEvents.addListener('filterupdate',callback);
+        },
+        removeListener: function (callback){
+            FilterStoreEvents.removeListener('filterupdate',callback);
+        },
+        currentFilter: function(item){
+            var flags = item.allergyflags;
+
+            function contains(flag)
+            {
+                return flags.indexOf(flag) !== -1;
+            }
+
+            if (filters.vegetarian){
+                if (!contains("vegetarian") && !contains("vegan"))
+                    return false;
+            }
+            if (filters.glutenfree){
+                if (contains("gluten"))
+                    return false;
+            }
+
+            return true;
+        },
+        getFilters: function(){
+            return filters;
+        },
+        setFilter: function(type,value){
+            filters[type] = value;
+            FilterStoreEvents.emitEvent('filterupdate');
+        }
+    }
+    
+}]);
