e3daa59bbcc4f9e1054542e5dd628b4c3088f902
Ethan Steinberg <ethan.steinberg@gmail.com>
2014-09-23 20:39:08 -0500

    Done?

--- app/static/js/components/serveryhours.js
+++ app/static/js/components/serveryhours.js
@@ -33,22 +33,7 @@ function dayofweek(input) {
   return day;
 }
 
-function twelvehour(input) {
-  if (input == undefined)
-      return ""
-  var hour = parseInt(input.substr(0,2));
-  var minutes = input.substr(3,2);
-  if (hour > 12){
-      hour = hour - 12;
-      return (hour.toString() + ":" + minutes + " PM");
- 
-  }
-  else {
-      return (finalString = hour.toString() + ":" + minutes + " AM");
-
-  }
-
-}
+var twelvehour = require('./utils').twelvehour
 
 
 module.exports = React.createClass({
