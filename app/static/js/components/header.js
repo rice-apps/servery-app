2a7e3560f7faf3fa15d76b3443daa87e8a9a4304
Ethan Steinberg <ethan.steinberg@gmail.com>
2015-01-14 09:28:40 -0600

    Fix isactive header

--- app/static/js/components/header.js
+++ app/static/js/components/header.js
@@ -14,19 +14,9 @@ var Link = ReactRouter.Link;
 var Tab = React.createClass({
 
   mixins: [ ActiveState ],
-
-  getInitialState: function () {
-    return { isActive: false };
-  },
-
-  updateActiveState: function () {
-    this.setState({
-      isActive: Tab.isActive(this.props.to, this.props.params, this.props.query)
-    })
-  },
-
+  
   render: function() {
-    var className = this.state.isActive ? 'active' : '';
+    var className = this.isActive(this.props.to, this.props.params, this.props.query) ? 'active' : '';
     var link = Link(this.props);
     return <li className={className}>{link}</li>;
   }
